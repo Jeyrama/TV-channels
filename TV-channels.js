fa6ebf82b80864caec922541d5af33a860bdcd34
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-02-18 14:06:20 -0800

    Create TV-channels.js

---
+++ TV-channels.js
@@ -0,0 +1,12 @@
+/*
+Program channels into your TV's memory. 
+An array with channels (strings) will be passed as an argument to the function redarr(). 
+Sort the channels in an alphabetical order, remove duplicates and, finally, 
+return an object where each channel (object's value) is assigned to a whole number (objects's key), starting with 0.
+
+Examples:
+  let arr = ["BBC1", "BBC2", "MTV"];
+  redarr(arr) // returns {"0":"BBC1", "1":"BBC2", "2":"MTV"}
+  let arr = ["BBC1", "BBC1", "BBC2", "MTV"];
+  redarr(arr) // returns {"0":"BBC1", "1":"BBC2", "2":"MTV"}
+*/
